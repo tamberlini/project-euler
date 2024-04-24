33db3995c91cdb9b6330889ad63c2e964896d743
vitor.tamberlini <vitor@mileiq.com>
2024-04-24 12:52:17 -0300

    Solved problem 4

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from problems.problem_1 import Problem as Problem_1
 from problems.problem_2 import Problem as Problem_2
 from problems.problem_3 import Problem as Problem_3
+from problems.problem_4 import Problem as Problem_4
 
 if __name__ == '__main__':
-    print(Problem_3().compute_answer())
+    print(Problem_4().compute_answer())
